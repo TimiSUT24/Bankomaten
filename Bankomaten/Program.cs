d0ecaa0274824d27a56c075367386f667c8c801d
TimiSUT24 <tim.petersenfalkenberg@outlook.com>
2024-10-01 19:59:13 +0200

    Lagt till välkomst meny

--- Bankomaten/Program.cs
+++ Bankomaten/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Välkommen till bankomaten");
+
+            Console.WriteLine("1: Logga in" +  
+                              "2: Exit");
+
+            int startMenu = int.Parse(Console.ReadLine());
+
+            switch (startMenu)
+            {
+                case 1:
+                    Login(); 
+                    break;
+                case 2:
+                    Console.WriteLine("Stänger bankomaten...");
+                    break;
+
+            }
         }
     }
 }
